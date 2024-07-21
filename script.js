1b8636df26dcde7a66ae9e62a5a526e03b3b38ec
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-07-21 22:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	 // Regular expression for validating email
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    
+    // Test the string against the regex pattern
+    return emailRegex.test(str);
 }
 
 // Do not change the code below.
